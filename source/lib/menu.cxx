d56d0220293bf60f9246517894b53a20d2fdf75b
michaelovalles <michaelj.ovalles@gmail.com>
2025-11-08 07:45:19 -0500

    removed unwanted lines

--- source/lib/menu.cxx
+++ source/lib/menu.cxx
@@ -1,26 +1,12 @@
-//#include "character.cxx"
-//#include <memory>
-// will me destorying pointer inside cause an error?
 
-//takes in input and of choice, results in action
-//static std::unique_ptr<Character> choice( std::string pick)
  Character* choice(std::string pick)
 {
-//error with release or sharing object reverts to orginal
 
-//std::unique_ptr<Character> chara;
-static Character chara; // = new Character;
+static Character chara; 
+static Rock rock; 
+static Paper paper;
+static Scissor scissor;
 
-//std::unique_ptr<Rock> rock;
-static Rock rock; // = new Rock; 
-
-//worked but deleted outside of this function
-static Paper paper; // = new Paper;
-//Paper* sendPaper = &paper;
-//std::unique_ptr<Paper> paper;
-
-static Scissor scissor;// = new Scissor;
-//std::unique_ptr<Scissor> scissor;
 if (pick == "rock") 
 {
 
